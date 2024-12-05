84244e067b4dfbb776871f9302d473882cf9ffaf
Damian <124635785+DendiLBN@users.noreply.github.com>
2024-12-05 08:39:49 +0100

    implement db drizzle-kit

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,14 +1,24 @@
+import { sql } from "drizzle-orm";
+
+import { db } from "@/db/index";
+
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
-export default function Home() {
+export default async function CreateInvoice() {
+  const results = await db.execute(sql`SELECT current_database();`);
+  console.log(results);
+
   return (
     <main className="flex flex-col justify-center h-full  gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-5xl font-bold">Create Invoice</h1>
         <p></p>
       </div>
+
+      {JSON.stringify(results)}
       <form className="grid gap-4 max-w-sm">
         <div>
           <Label htmlFor="name" className="block font-semibold mb-2">
@@ -32,7 +42,7 @@ export default function Home() {
           <Label htmlFor="desciption" className="block font-semibold mb-2">
             Description
           </Label>
-          <Input id="description" type="text" />
+          <Textarea id="description" name="description"></Textarea>
         </div>
         <div>
           <Button className="w-full font-semibold">Submit</Button>
