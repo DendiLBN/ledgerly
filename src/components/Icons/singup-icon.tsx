c90f8ffd81330c20d3242fada1b56b2ea5380ef6
Damian <124635785+DendiLBN@users.noreply.github.com>
2024-12-20 23:48:27 +0100

    hotfix svg for landing page signup

--- src/components/Icons/singup-icon.tsx
+++ src/components/Icons/singup-icon.tsx
@@ -6,9 +6,9 @@ export const SignUpIcon = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="mx-auto size-10"
   >
     <circle cx="12" cy="12" r="10" />
