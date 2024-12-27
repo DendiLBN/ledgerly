9d1c8c2c1ccaa2968fd2c892144b0f94ff84cefd
Damian <124635785+DendiLBN@users.noreply.github.com>
2024-12-27 22:41:55 +0100

    implement authenticator application for sign in

--- src/app/sign-in/[[...sing-in]]/page.tsx
+++ src/app/sign-in/[[...sing-in]]/page.tsx
@@ -1,14 +1,15 @@
 "use client";
+import Link from "next/link";
 
 import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
 
 import { View } from "@/components/common/view";
 import { Text } from "@/components/common/text";
-import { SubmitButton } from "@/components/SubmitButton";
 import { SignInIcon } from "@/components/Icons/singup-icon";
-import Link from "next/link";
 import { GoogleLogo } from "@/components/Icons/google-logo";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
 
 export default function SignInPage() {
   return (
@@ -57,7 +58,7 @@ export default function SignInPage() {
             </View>
           </View>
           <Text className="text-center text-sm text-neutral-500">
-            Don&apos;t have an account?{" "}
+            Don&apos;t have an account?
             <Link
               href={String(process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL)}
               className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100"
@@ -71,7 +72,79 @@ export default function SignInPage() {
           title="Verify your account"
           className="w-full space-y-6 rounded-2xl px-4 py-10 sm:w-96 sm:px-8"
         >
-          <SubmitButton />
+          <SignIn.Strategy name="email_code">
+            <View className="text-center">
+              <SignInIcon />
+              <Text className="mt-4 text-xl font-medium tracking-tight text-neutral-950">
+                Verify email code
+              </Text>
+            </View>
+            <Clerk.GlobalError className="block text-sm text-red-600" />
+            <Clerk.Field name="code">
+              <Clerk.Label className="sr-only">Email code</Clerk.Label>
+              <Clerk.Input
+                type="otp"
+                required
+                placeholder="Email code"
+                className="w-full border-b border-neutral-200 bg-white pb-2 text-sm/6 text-neutral-950 outline-none placeholder:text-neutral-400 hover:border-neutral-300 focus:border-neutral-600 data-[invalid]:border-red-600 data-[invalid]:text-red-600"
+              />
+              <Clerk.FieldError className="mt-2 block text-xs text-red-600" />
+            </Clerk.Field>
+            <SignIn.Action
+              submit
+              className="relative w-full rounded-md bg-neutral-600 bg-gradient-to-b from-neutral-500 to-neutral-600 py-1.5 text-sm text-white shadow-[0_1px_1px_0_theme(colors.white/10%)_inset,0_1px_2.5px_0_theme(colors.black/36%)] outline-none ring-1 ring-inset ring-neutral-600 before:absolute before:inset-0 before:rounded-md before:bg-white/10 before:opacity-0 hover:before:opacity-100 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 active:bg-neutral-600 active:text-white/60 active:before:opacity-0"
+            >
+              Continue
+            </SignIn.Action>
+          </SignIn.Strategy>
+          <SignIn.Strategy name="totp">
+            <View className="text-center">
+              <SignInIcon />
+              <Text className="mt-4 text-xl font-medium tracking-tight text-neutral-950">
+                Verify Authenticator Code
+              </Text>
+            </View>
+            <Clerk.GlobalError className="block text-sm text-red-600" />
+            <Clerk.Field name="code">
+              <Clerk.Label className="sr-only">Authenticator Code</Clerk.Label>
+              <Clerk.Input
+                type="otp"
+                required
+                className="flex justify-center gap-1"
+                render={({ value, status }) => (
+                  <View
+                    data-status={status}
+                    className="relative h-9 w-8 rounded-md bg-white ring-1 ring-inset ring-zinc-300 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
+                  >
+                    <AnimatePresence>
+                      {value && (
+                        <motion.span
+                          initial={{ opacity: 0, scale: 0.75 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, scale: 0.75 }}
+                          className="absolute inset-0 flex items-center justify-center text-zinc-950"
+                        >
+                          {value}
+                        </motion.span>
+                      )}
+                      {value}
+                    </AnimatePresence>
+                    {status === "cursor" && (
+                      <motion.div
+                        layoutId="otp-input-focus"
+                        transition={{ ease: [0.2, 0.4, 0, 1], duration: 0.2 }}
+                        className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
+                      />
+                    )}
+                  </View>
+                )}
+              />
+              <Clerk.FieldError className="mt-2 block text-xs text-red-600" />
+            </Clerk.Field>
+            <SignIn.Action submit asChild>
+              <Button className="w-full font-bold">Log In</Button>
+            </SignIn.Action>
+          </SignIn.Strategy>
         </SignIn.Step>
       </SignIn.Root>
     </View>
